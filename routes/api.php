b9d3e50c06f3b26c0873167bfa2cf89212985cbb
Omar-Codess <omartaki8@gmail.com>
2023-01-19 17:57:48 +0100

    testata chiamata api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
-    return response()->json([
-        'success' => true,
-        'results' => [
-            'name' => 'Omar',
-            'state' => 'Italia'
-        ]
-    ]);
-});
+Route::get('/projects', [ProjectController::class, 'index']);
